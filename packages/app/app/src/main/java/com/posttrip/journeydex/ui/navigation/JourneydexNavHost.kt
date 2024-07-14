58a3ff0dd4cc0289c20dd401ad254f5797464b0d
Jung SeokJoon <eshc.joon@gmail.com>
2024-07-14 14:55:11 +0900

    add(app): main navigation

---
+++ packages/app/app/src/main/java/com/posttrip/journeydex/ui/navigation/JourneydexNavHost.kt
@@ -0,0 +1,13 @@
+package com.posttrip.journeydex.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+@Composable
+fun JourneydexNavHost(
+    navController: NavHostController,
+    modifier : Modifier = Modifier,
+) {
+
+}
