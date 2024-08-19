4e44fd78b8404bcf97f6291111154bbf3ac845cc
woo.dong <woo.dong@navercorp.com>
2024-08-20 00:35:07 +0900

    feat(server): 온보딩 저장 API 구현하기

--- packages/server/src/service/userService.ts
+++ packages/server/src/service/userService.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { IUser, User } from '../schema';
+import { IUser, User, IOnboarding, Onboarding } from '../schema';
 
 export const login = async (req: express.Request<any, any, IUser>, res: express.Response) => {
   try {
@@ -17,7 +17,57 @@ export const login = async (req: express.Request<any, any, IUser>, res: express.
       user.save();
     }
 
-    res.status(200).json({ id, nickname, onboarding: true });
+    const onboarding = await Onboarding.findOne({ id });
+
+    res.status(200).json({ id, nickname, onboarding: Boolean(onboarding) });
+  } catch (error: any) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const travelStyleKeywords = ['healing', 'culture', 'gourmet', 'activity'];
+const destinationTypeKeywords = ['beach', 'mountain', 'city', 'island'];
+const travelTypeKeywords = ['solo', 'family', 'couple', 'friends'];
+
+const validateKeywords = (keywords: IOnboarding['keywords']) => {
+  const { travelStyleKeyword, destinationTypeKeyword, travelTypeKeyword } = keywords;
+
+  const isValidTravelStyle = travelStyleKeywords.includes(travelStyleKeyword);
+  const isValidDestinationType = destinationTypeKeywords.includes(destinationTypeKeyword);
+  const isValidTravelType = travelTypeKeywords.includes(travelTypeKeyword);
+
+  return isValidTravelStyle && isValidDestinationType && isValidTravelType;
+};
+
+export const saveOnboarding = async (req: express.Request<any, any, IOnboarding>, res: express.Response) => {
+  try {
+    const { id, keywords } = req.body;
+
+    const userExisted = await User.findOne({ id });
+
+    if (!userExisted) {
+      return res.status(500).json({ message: 'User information not found' });
+    }
+
+    if (!validateKeywords(keywords)) {
+      return res.status(500).json({ message: 'Invalid travelStyleKeyword' });
+    }
+
+    const onboardingExisted = await Onboarding.findOne({ id });
+
+    if (onboardingExisted) {
+      return res.status(500).json({ message: 'Onboarding already exists' });
+    }
+
+    const onboarding = new Onboarding({
+      id,
+      keywords,
+    });
+
+    onboarding.save();
+
+    res.status(200).json({ id, keywords });
   } catch (error: any) {
     console.log(error);
     res.status(500).json({ message: error.message });
